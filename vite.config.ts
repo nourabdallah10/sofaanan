b2dd885647d631eb06febe75b26d7915b35622d4
nourabdallah10 <noorabdallah40@gmail.com>
2025-12-20 01:39:12 +0300

    Fix: Change base path to / for custom domain sofa-anan.com

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sofaanan/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
