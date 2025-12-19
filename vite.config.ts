2947f5eea1e0facd37445167d1174e917ece2632
nourabdallah10 <noorabdallah40@gmail.com>
2025-12-19 22:49:52 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sofaanan/',
   plugins: [react()],
   resolve: {
     alias: {
